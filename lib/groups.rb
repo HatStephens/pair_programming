8a51311ab8c8e08233a18d653e0b247afe330b83
Stephen Giles <s.giles@hotmail.co.uk>
2014-09-22 23:58:12 +0100

    Steve's attempt at speaking pairs (with list too). Plus a lame group program.

--- lib/groups.rb
+++ lib/groups.rb
@@ -1,16 +1,17 @@
-# say = 'say -v fiona --rate=20 '
+#say = 'say -v fiona --rate=20 '
 
 names = %w[ Alex AndrewHercules AndrewHarrison AndrewSnead AnaNogal AnnaJ Bernard Camilla Colin Craig Dan Denise Elena Ella Fadie Hadi James Karin Nicole Pablo Rachel Sandrine Sean Steve Tiiim Victoria Yvette Zeeshan]
 
 names.shuffle!
 
-a=0
-b=1
-
-while b<28 do
-  `say #{names[a.to_i]} pair with #{names[b.to_i]}`
-  puts "#{names[a.to_i]} pair with #{names[b.to_i]}"
-  sleep 0
-  a += 2
-  b += 2
-end
+puts names[0..4]
+puts '---'
+puts names[5..9]
+puts '---'
+puts names[10..15]
+puts '---'
+puts names[16..21]
+puts '---'
+puts names[22..27]
+puts '------'
+puts '------'
